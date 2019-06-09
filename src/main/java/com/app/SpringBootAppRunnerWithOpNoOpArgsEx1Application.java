a3b33227a9585affd3250659416db21dce38012b
yogendradixit86 <dixitshloke7@gmail.com>
2019-06-09 15:04:29 +0530

    Revert "initial setup"
    This reverts commit bee2b80f90c44feece8cb8af5302aa1ad1a8fef5.

--- src/main/java/com/app/SpringBootAppRunnerWithOpNoOpArgsEx1Application.java
+++ src/main/java/com/app/SpringBootAppRunnerWithOpNoOpArgsEx1Application.java
@@ -8,6 +8,6 @@ public class SpringBootAppRunnerWithOpNoOpArgsEx1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootAppRunnerWithOpNoOpArgsEx1Application.class, args);
-	} 
+	}
 
 }
